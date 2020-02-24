1ca5db07a5a40b90cd5c459765c582ec8103569f
Jeroenvdv1993 <jeroen.vanderveken@hotmail.com>
2020-02-24 10:54:05 +0100

    fix

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname));
+
 app.listen(app.get('port'), function(){
     console.log('Node server is running on port ' + app.get('port'));
 })
