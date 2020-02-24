8b29ed5828e2bf6c94b2d90b6cbb8853a31c3e52
Jeroenvdv1993 <jeroen.vanderveken@hotmail.com>
2020-02-24 11:15:35 +0100

    Fix

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 5000;
+const INDEX = "/index.html";
 
-app.set('port', (process.env.PORT || 5000));
+const server = express()
+.use((req, res) => res.sendFile(INDEX, {root: __dirname}))
+.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-app.listen(app.get('port'), function(){
-    console.log('Node server is running on port ' + app.get('port'));
-})
+const io = socketIO(server);
 
-app.get('/', function(req, res){
-    res.render('index.ejs');
-});
-
-io.sockets.on('connection', function(socket){
+io.on('connection', (socket) =>{
+    console.log('Client connected');
+    
     socket.on('username', function(username){
         socket.username = username;
         io.emit('is_online', '<i>' + socket.username + ' join the chat...</i>');
@@ -26,9 +24,5 @@ io.sockets.on('connection', function(socket){
     socket.on('chat_message', function(message){
         io.emit('chat_message', '<strong>' + socket.username +"</strong>: " + message);
     });
-})
-
-const server = http.listen(8080, function(){
-    console.log('listening on *:8080');
 })
 
