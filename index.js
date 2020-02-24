c4e373c1ec403aedf63cd0108ef609696454c4b6
Jeroenvdv1993 <jeroen.vanderveken@hotmail.com>
2020-02-24 14:00:24 +0100

    added list of users

--- index.js
+++ index.js
@@ -9,22 +9,26 @@ const server = express()
 
 const io = socketIO(server);
 
+var clients = [];
+
 io.on('connection', (socket) =>{
-    console.log('Client connected');
-    
     socket.on('username', function(username){
         socket.username = username;
+        clients.push(username);
         var today = new Date();
         var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
         var print = '<strong>[' + time + ']</strong>' + '<i>' + socket.username + ' joined the chat...</i>';
         io.emit('is_online', print);
+        io.emit('clients', clients);
     });
 
     socket.on('disconnect', function(username){
+        clients.splice(clients.indexOf(username), 1);
         var today = new Date();
         var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
         var print = '<strong>[' + time + ']</strong>' + '<i>' + socket.username + ' left the chat...</i>';
         io.emit('is_online', print);
+        io.emit('clients', clients);
     });
 
     socket.on('chat_message', function(message){
